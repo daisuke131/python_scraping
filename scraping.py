8d644475458d07888e31102284d866f12d64c66e
Daisuke <kurimotodaisuke@DaisukenoMacBook-Air.local>
2021-07-14 17:09:32 +0900

    drive設定処理を別ファイルで管理

--- scraping.py
+++ scraping.py
@@ -1,59 +1,22 @@
-import os
 from pathlib import Path
 from time import sleep
 
 import pandas as pd
 from dotenv import load_dotenv
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.firefox import GeckoDriverManager
-from webdriver_manager.utils import ChromeType
 
+from driver_setting import set_driver
 from log_setting import log_setting
 
 log = log_setting()
 load_dotenv()
 
 
-def set_driver(driver_path, headless_flg):
-    if "firefox" in driver_path:
-        options = webdriver.FirefoxOptions()
-    else:
-        options = webdriver.ChromeOptions()
-
-    # ヘッドレスモード（画面非表示モード）をの設定
-    if headless_flg is True:
-        options.add_argument("--headless")
-
-    # 起動オプションの設定
-    options.add_argument(
-        "--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
-        + "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36"
-    )
-    options.add_argument("--ignore-certificate-errors")
-    options.add_argument("--ignore-ssl-errors")
-    options.add_argument("--incognito")
-
-    # firefox or chromium指定以外はchromeで起動
-    if "firefox" in driver_path:
-        return webdriver.Firefox(
-            executable_path=GeckoDriverManager().install(), options=options
-        )
-    elif "chromium" in driver_path:
-        return webdriver.Chrome(
-            ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install(),
-            options=options,
-        )
-    else:
-        return webdriver.Chrome(ChromeDriverManager().install(), options=options)
-
-
 def main():
     # ログ出力設定
     log.debug("===== start =====")
 
-    # 使用ブラウザの管理は.envで
-    driver = set_driver(os.getenv("BROWSER"), True)
+    # ヘッドレスフラグを渡す。Trueならブラウザを立ち上げない。
+    driver = set_driver(True)
     driver.get("https://job.mynavi.jp/2022/")
     sleep(3)
 
@@ -128,6 +91,7 @@ def main():
     driver.quit()
 
 
+# CSV書き込み
 def write_csv(search_word, corp_data_list):
     # ディレクトリがないとエラーになるため作成
     dir = Path("./csv")
@@ -142,6 +106,7 @@ def write_csv(search_word, corp_data_list):
     df.to_csv(csv_path, index=False, encoding="CP932")
 
 
+# 会社データ取得
 def get_corp_data(driver):
     # get_data_judgeでエラーならpass
     corp_name = get_data_judge(
@@ -157,6 +122,7 @@ def get_corp_data(driver):
     return [corp_name, corp_income, corp_work_hours, corp_URL]
 
 
+# データ取得できない場合はスルー
 def get_data_judge(driver, data_name, xpath):
     try:
         return driver.find_element_by_xpath(xpath).text
